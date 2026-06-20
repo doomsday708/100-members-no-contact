25cc77c3c2a6076ad5a886a144cc2912145abb5f
Sanket <chaudharisanket2000@gmail.com>
2026-06-20 19:09:03 +0530

    feat: change screen using process_mode instead of visible

--- Actual Game Folder/scripts/globals.gd
+++ Actual Game Folder/scripts/globals.gd
@@ -13,14 +13,20 @@ var SCREENS = [
 
 func _hide_all_screens():
 	SCREENS.map(func(s):
-		get_node(s).visible = false
+		# Reference for 'process_mode'
+		# Link: https://forum.godotengine.org/t/how-to-disable-enable-a-node/22387/6
+		get_node(s).process_mode = 4 # Disabled
+		get_node(s).hide()
 	)
 
 func _show_screen(screen_path: String):
-	get_node(screen_path).visible = true
+	get_node(screen_path).process_mode = 0 # Inherit
+	get_node(screen_path).show()
 
 # Whenever we want to change the screen, menu to gameplay or whatever
-# ALWAYS USE THIS
+# We'll use this, but issue with this is, this will not have any animation
+# If someone wants to, they can maybe hack around this, or better state machine
+# for changing screens
 func change_screen(screen_path: String):
 	_hide_all_screens()
 	_show_screen(screen_path)
